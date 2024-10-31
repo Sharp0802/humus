c08b96fbbddff842f14bdb8874d437a72f0e5e0e
Sharp0802 <sharp0802.business@gmail.com>
2024-10-31 17:22:35 +0900

    :recycle: Correct visibility of exported struct

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,9 @@
 use std::fmt::{Debug, Display, Formatter};
 
-pub(crate) struct Error {
+///
+/// A struct that represents generic errors
+///
+pub struct Error {
     message: String
 }
 
@@ -19,10 +22,16 @@ impl Display for Error {
 impl std::error::Error for Error {}
 
 impl Error {
+    ///
+    /// Create new error from message string
+    ///
     pub fn new(message: &str) -> Self {
         Self{ message: message.to_string() }
     }
 
+    ///
+    /// Create new error from to-string-able structs
+    ///
     pub fn from<T: ToString>(value: T) -> Self {
         Self{ message: value.to_string() }
     }
